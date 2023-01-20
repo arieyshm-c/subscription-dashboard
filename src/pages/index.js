bcbe3e5ef747b97aabe4fcef8ecab12f4cbc2545
arieyshma chowhan <arieyshmachowhan21@gmail.com>
2023-01-20 16:30:02 +0530

    neww

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
-import { DeleteModal } from "./DeleteModalpage";
+import { DeleteModal } from "../components/DeleteModal";
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
